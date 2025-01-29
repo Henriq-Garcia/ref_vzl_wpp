9d135ca4e825d10af79424ea4506459ffd0174b0
Henriq-Garcia <botzinso@gmail.com>
2025-01-29 17:28:50 -0300

    set server test

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import "./config/config"
+import { BaileysConnector } from "./classes/baileysConnector"
+
+(async () => {
+    const baileys = new BaileysConnector("17997819563")
+    await baileys.init(true)
+})()
