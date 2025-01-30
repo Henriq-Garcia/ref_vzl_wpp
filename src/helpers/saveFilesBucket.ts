7bf2eff11de64d32d012ec4f4cdbf63c28752137
Henriq-Garcia <botzinso@gmail.com>
2025-01-30 14:49:49 -0300

    add authenticateCobCo function

--- src/helpers/saveFilesBucket.ts
+++ src/helpers/saveFilesBucket.ts
@@ -1,4 +1,20 @@
-import axios from "axios"
+import axios from "axios";
+
+async function authenticateCobCo() {
+    const payload = {
+        cpf: cobcoUser,
+        senha: cobcoPass,
+        projeto: cobcoProjeto,
+        at_cliente: cobcoCliente
+    };
+
+    try {
+        const response = await axios.post(`https://${cobcoUrl}:10740/api/dados-usuario`, payload);
+        global.cobcoToken = response.data.token;
+    } catch (error) {
+        console.error("Erro ao autenticar:", error);
+    }
+}
 
 export async function postFileToBucket(fileName: string, fileBase64: string, folder: string) {
     const payload = {
@@ -6,7 +22,7 @@ export async function postFileToBucket(fileName: string, fileBase64: string, fol
         bucket: "corban-vazoli",
         diretorio_arquivo: folder,
         conteudo_arquivo_b64: fileBase64
-    }
+    };
 
     try {
         const response = await axios.post(`https://${cobcoUrl}:10740/api/s3/arquivos`, payload, {
@@ -14,9 +30,26 @@ export async function postFileToBucket(fileName: string, fileBase64: string, fol
                 Authorization: cobcoToken,
                 "Content-Type": "application/json"
             }
-        })
-        return { error: false, content: response.data }
-    } catch (error) {
+        });
+        return { error: false, content: response.data };
+    } catch (error: any) {
+        if (error.response && error.response.status === 401) {
+            console.warn("Token expirado, autenticando novamente...");
+            await authenticateCobCo();
+
+            try {
+                const retryResponse = await axios.post(`https://${cobcoUrl}:10740/api/s3/arquivos`, payload, {
+                    headers: {
+                        Authorization: cobcoToken,
+                        "Content-Type": "application/json"
+                    }
+                });
+                return { error: false, content: retryResponse.data };
+            } catch (retryError) {
+                return { error: true, message: retryError };
+            }
+        }
+
         return { error: true, message: error };
     }
 }
