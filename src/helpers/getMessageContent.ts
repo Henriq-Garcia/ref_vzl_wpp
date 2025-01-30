820d22e81f88ec2d11a46d708ceaf86a6a72d54a
Henriq-Garcia <botzinso@gmail.com>
2025-01-30 11:59:14 -0300

    ref: format code

--- src/helpers/getMessageContent.ts
+++ src/helpers/getMessageContent.ts
@@ -1,6 +1,7 @@
 import { WAMessage, assertMediaContent } from "@whiskeysockets/baileys";
 import { extension } from "mime-types";
 import { fileToBase64 } from "./convertFileToBase64";
+import { thumbnailBase64 } from "./canvasThumbnail";
 
 export async function getMessageContent(message: WAMessage, messageId: number): Promise<any> {
     try {
@@ -16,42 +17,63 @@ export async function getMessageContent(message: WAMessage, messageId: number): 
 
         switch (messageType) {
             case "conversation":
-                return { mensagem: messageContent.conversation }
+                return { 
+                    mensagem: messageContent.conversation 
+                }
 
             case "extendedTextMessage":
-                return { mensagem: messageContent.extendedTextMessage?.text }
+                return { 
+                    mensagem: messageContent.extendedTextMessage?.text 
+                }
 
             case "imageMessage":
                 content = assertMediaContent(messageContent)
                 fileExtension = extension(content.mimetype)
                 base64Anexo = await fileToBase64(content, "image")
-                // base64Thumbnail = await fileToBase64(content, "thumbnail-image")
-                return { mensagem: messageContent.imageMessage.caption, anexo: `data:${content.mimetype};base64,${base64Anexo}`,  }
+                base64Thumbnail = await thumbnailBase64(base64Anexo, "image")
+                return { 
+                    mensagem: messageContent.imageMessage.caption, 
+                    anexo: base64Anexo, 
+                    thumbnail: base64Thumbnail 
+                }
 
             case "videoMessage":
                 content = assertMediaContent(messageContent)
                 fileExtension = extension(content.mimetype)
                 base64Anexo = await fileToBase64(content, "video")
-                return { mensagem: messageContent.videoMessage.caption, anexo: `data:${content.mimetype};base64,${base64Anexo}`,  }
+                base64Thumbnail = await thumbnailBase64(base64Anexo, "video")
+                return { 
+                    mensagem: messageContent.videoMessage.caption,
+                    anexo: base64Anexo, 
+                    thumbnail: base64Thumbnail 
+                }
 
             case "stickerMessage":
                 content = assertMediaContent(messageContent)
                 fileExtension = extension(content.mimetype)
                 base64Anexo = await fileToBase64(content, "sticker")
-                return { anexo: `data:${content.mimetype};base64,${base64Anexo}`}
+                return { 
+                    anexo: base64Anexo
+                }
 
             case "documentMessage":
                 content = assertMediaContent(messageContent)
                 fileExtension = extension(content.mimetype)
                 base64Anexo = await fileToBase64(content, "document")
-                // base64Thumbnail = await fileToBase64(content, "thumbnail-document")
-                return { mensagem: messageContent.documentMessage.caption, anexo: `data:${content.mimetype};base64,${base64Anexo}`,  }
+                base64Thumbnail = await thumbnailBase64(base64Anexo, "document", content.mimetype)
+                return { 
+                    mensagem: messageContent.documentMessage.caption, 
+                    anexo: base64Anexo,
+                    thumbnail: base64Thumbnail
+                }
             
             case "audioMessage":
                 content = assertMediaContent(messageContent)
                 fileExtension = extension(content.mimetype)
                 base64Anexo = await fileToBase64(content, "audio")
-                return { anexo: `data:${content.mimetype};base64,${base64Anexo}` }
+                return { 
+                    anexo: base64Anexo
+                }
         }
     } catch (error) {
         console.error(error)
